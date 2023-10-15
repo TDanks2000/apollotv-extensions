bafb35b7ba9d7f7b9ff586f7117976efe04e8876
Tommy <tommydanks2000@outlook.com>
2023-10-15 13:29:14 +0100

    enable allAnime

--- src/extensions/anime/index.ts
+++ src/extensions/anime/index.ts
@@ -1,5 +1,11 @@
 import Gogoanime from "./gogoanime";
 import AnimePahe from "./animepahe";
 import Kickassanime from "./kickassanime";
+import AllAnime from "./allanime";
 
-export default { Gogoanime, AnimePahe, Kickassanime };
+export default {
+  AllAnime,
+  Gogoanime,
+  AnimePahe,
+  Kickassanime,
+};
--- src/utils/extensions-list.ts
+++ src/utils/extensions-list.ts
@@ -4,7 +4,7 @@ import { ANIME, INFO, MANGA, MOVIE } from "../extensions";
  * Enabled Extensions
  */
 export const EXTENSION_LIST = {
-  ANIME: [new ANIME.Gogoanime(), new ANIME.AnimePahe()],
+  ANIME: [new ANIME.Gogoanime(), new ANIME.AnimePahe(), new ANIME.AllAnime()],
   MANGA: [new MANGA.MangaPill(), new MANGA.Mangasee123()],
   INFO: [new INFO.Anilist()],
   MOVIE: [new MOVIE.FlixHQ()],
